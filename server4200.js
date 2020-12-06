1d1ad54014c6aa952a88fc3737e16957c85a11d4
TatianaUsanina <63223475+TatianaUsanina@users.noreply.github.com>
2020-12-06 16:39:28 +0500

    init commit

---
+++ server4200.js
@@ -0,0 +1,51 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var server = new http.Server();
+server.listen(4200, '127.0.0.1');
+
+server.on('request', function(req, res){
+    var urlParsed = url.parse(req.url, true);
+    console.log(urlParsed);
+    console.log(req.headers);
+    
+    if(urlParsed.pathname == '/img'){
+        fs.readFile("img.jpg", function(err, data){
+            if(err){
+                console.error(err.message);
+            }else{
+                res.end(data);
+            }
+        });
+    };
+
+    if(urlParsed.pathname == '/otherimg'){
+        fs.readFile("otherimg.jpg", function (err, data){
+            if(err){
+                console.error(err.message);
+            }else{
+                res.end(data);
+            }
+        });
+    };
+
+    if(urlParsed.pathname == '/content'){
+        fs.readFile("content.txt", {encoding: 'utf-8'}, function(err, data){
+            if(err){
+                console.error(err.message);
+            }else{
+                res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+                res.setHeader("Access-Control-Allow-Credentials", "true");
+                res.setHeader("Access-Control-Allow-Headers","Origin, Content-Type, X-Auth-Token, Authorization");
+                res.end(data);
+            }
+        });
+    };
+
+    if(urlParsed.pathname == '/customer/13'){
+        var callback = urlParsed.query.callback;
+        console.log(callback);
+        res.end(callback + '(' + JSON.stringify({"Name": "Adam", "Id": 13, "Rank": 1}) + ');');
+    }
+ });
